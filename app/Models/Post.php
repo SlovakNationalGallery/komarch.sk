c419e1dbb5392efd4b0daefeea9aad6ed639ca6f
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-12-07 04:23:40 +0100

    add wp:import command + migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Spatie\Tags\HasTags;
+use Spatie\Tags\Tag;
+
+class Post extends Model
+{
+    use HasFactory,
+        HasSlug,
+        HasTags;
+
+    public $with = ['tags'];
+
+    public $dates = ['publish_date'];
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getWordpressFullUrlAttribute(): string
+    {
+        return "/{$this->wp_post_name}";
+    }
+
+    public function getUrlAttribute(): string
+    {
+        return action('\App\Http\Controllers\PostsController@show', $this->slug);
+    }
+
+}
---
+++ database/migrations/2020_12_07_022021_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('text');
+            $table->string('wp_post_name')->nullable();
+            $table->datetime('publish_date');
+            $table->boolean('published')->default(false);
+            $table->string('author')->default('komarch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
