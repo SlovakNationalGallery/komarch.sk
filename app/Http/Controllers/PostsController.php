ad34327304862845c7fab681befe21d9365d852a
Igor Rjabinin <igor.rjabinin@gmail.com>
2020-12-07 05:15:46 +0100

    simple frontend for posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,8 +7,14 @@ use App\Models\Post;
 
 class PostsController extends Controller
 {
+    public function index(Request $request)
+    {
+        $posts = Post::published()->orderBy('published_at', 'desc')->paginate(10);
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post)
     {
-        return $post->title; //view('posts.show', compact('post'));
+        return view('posts.show', compact('post'));
     }
 }
