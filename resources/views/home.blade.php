2a6e10e43232663d8c5f86ba691d667ae7c0d904
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-02-15 10:18:41 +0100

    add component tender-small

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,9 @@
 
 @include('components.notification_bar')
 
-<div class="container py-4">
+<div class="container py-5">
     <div class="row">
-        <div class="col-md-8 ">
+        <div class="col-md-8 border-right">
             <h1>Podporujeme rozvoj architektúry na&nbsp;Slovensku</h1>
             <p>Slovenská Komora Architektov je odbornou organizáciou, ktorá sa zameriava na vzdelávanie a informovanie verejnosti o potrebách kvalitnej architektúry</p>
 
@@ -40,7 +40,32 @@ adipiscing',
             </div>
         </div>
         <div class="col-md-4 ">
-            Súťaže
+
+            <h5 class="display-5">Súťaže</h5>
+            <ul class="nav justify-content-between">
+                <li class="nav-item">
+                    <a class="nav-link p-0 active" href="#">Ukončené</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-0" href="#">Prebiehajúce</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-0" href="#">Pripravované</a>
+                </li>
+            </ul>
+
+            <div class="tenders mb-2">
+                @for ($i = 0; $i < 4; $i++)
+                    @include('components.tender-small', [
+                        'date' => '11. 1. 2021 – 10. 12. 2020',
+                        'text' => 'Výsledky krajinársko-urbanistickej súťaže Revitalizácia Mlynského náhonu v Košiciach',
+                        'url' => '#',
+                    ])
+                @endfor
+            </div>
+            <div>
+                <a href="#">Viac súťaží →</a>
+            </div>
         </div>
     </div>
     <div class="row">
