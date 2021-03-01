dc16488c862f1f0a4ce315e0789016f97ca0f5c0
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-03-02 00:06:46 +0100

    header with routes

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">S K A</a>
+    <a class="navbar-brand" href="{{ route('home') }}">S K A</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbar1">
       <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Aktuality</a>
+        <li class="nav-item {{ Route::is('posts.*') ? 'active' : '' }}">
+          <a class="nav-link" href="{{ route('posts.index') }}">Aktuality</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Register diel</a>
