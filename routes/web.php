84f065bbae8034628553b3334fa99bd4522909a8
Igor Rjabinin <igor.rjabinin@gmail.com>
2021-03-02 00:58:28 +0100

    design search page accroding the wireframe

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 
     Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
-    Route::get('/search', 'App\Http\Controllers\SearchController@index');
+    Route::get('/search', 'App\Http\Controllers\SearchController@index')->name('search');
 
     Route::resource('spravy', '\App\Http\Controllers\PostsController')->names('posts')
                 ->parameter('spravy', 'post');
