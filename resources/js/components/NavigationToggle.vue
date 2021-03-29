b8bace608c9a9b31ed8aeb0b7308801fb6f328ed
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-29 13:12:06 +0200

    Vue setup added with a dummy component

---
+++ resources/js/components/NavigationToggle.vue
@@ -0,0 +1,15 @@
+<template>
+    <button @click="toggled = !toggled">
+        {{ toggled ? 'Toggled' : 'Not Toggled'}}
+    </button>
+</template>
+
+<script>
+  export default {
+      data() {
+          return {
+              toggled: false
+          }
+      },
+  }
+</script>
--- resources/views/components/header/header-navigation.blade.php
+++ resources/views/components/header/header-navigation.blade.php
@@ -1,4 +1,5 @@
 <nav class="px-4 pb-4 border rounded-2xl md:border-0 md:flex-1 md:flex md:pl-20 md:mt-10">
+    <navigation-toggle></navigation-toggle>
     @include('components.header.header-navigation-toggle')
     <div class="flex-1 md:mx-10">
         @include('components.header.header-navigation-list', ['navigation_items' => $navigation_items_1])
