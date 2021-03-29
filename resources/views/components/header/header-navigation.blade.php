5bfcb1ed14d0d38f90a60b911e477952c4c4837d
Andrej Bilec <andrej.bilec@gmail.com>
2021-03-29 11:10:34 +0200

    Basic search added to the menu

--- resources/views/components/header/header-navigation.blade.php
+++ resources/views/components/header/header-navigation.blade.php
@@ -1,7 +1,9 @@
-<nav class="md:flex md:flex-1 md:justify-around lg:px-20 lg:mr-20">
-    <div>
+<nav class="md:flex md:flex-1 md:pl-20 md:mt-10">
+    <div class="flex-1 md:mx-10">
         @include('components.header.header-navigation-list', ['navigation_items' => $navigation_items_1])
         @include('components.header.header-navigation-search')
     </div>
-    @include('components.header.header-navigation-list', ['navigation_items' => $navigation_items_2])
+    <div class="flex-1 md:mx-10">
+        @include('components.header.header-navigation-list', ['navigation_items' => $navigation_items_2])
+    </div>
 </nav>
