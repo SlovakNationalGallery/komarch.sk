8882b72d5a179238e2939c335345aa895dee32ba
Ajo Bilec <andrej.bilec@gmail.com>
2021-03-30 15:05:17 +0200

    Active link check fixed
    Co-authored-by: Igor Rjabinin <igor.rjabinin@gmail.com>

--- resources/views/components/link-arrow.blade.php
+++ resources/views/components/link-arrow.blade.php
@@ -1,6 +1,6 @@
 <a
     href="{{ $link['url'] }}"
-    class="group block leading-loose flex {{ Request::is($link['url']) ? 'text-blue' : '' }}"
+    class="group block leading-loose flex {{ (url()->current() == $link['url']) ? 'text-blue' : '' }}"
 >
     {{ $link['title'] }}
     <span class="opacity-0 transform group-hover:opacity-100 group-hover:translate-x-4 duration-200">
