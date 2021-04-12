96a17e4ddf9b764df65549b1de90473bf4bf7f96
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-12 10:23:22 +0200

    Basic newsletter implementation added

--- resources/views/components/footer/footer-newsletter.blade.php
+++ resources/views/components/footer/footer-newsletter.blade.php
@@ -1,3 +1,19 @@
-<div>
-    Newsletter
-</div>
+<form class="flex items-center flex-wrap">
+    <label
+        for="newsletter-email"
+        class="text-xl leading-snug mr-4"
+    >
+        Prihláste sa na odber nášho newslettra
+    </label>
+    <input
+        type="email"
+        id="newsletter-email"
+        placeholder="zadajte váš e-mail"
+        class="border-b flex-1 focus:outline-none h-8"
+    >
+    <input
+        type="submit"
+        value="odoslať ->"
+        class="bg-transparent text-xl cursor-pointer px-4 focus:outline-none"
+    >
+</form>
