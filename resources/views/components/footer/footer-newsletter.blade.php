f0a5cda78cf7332f4412d633ab496ee4f67e1e4c
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-12 12:53:48 +0200

    Submit button hover arrow style added

--- resources/views/components/footer/footer-newsletter.blade.php
+++ resources/views/components/footer/footer-newsletter.blade.php
@@ -12,9 +12,10 @@
         minlength="6"
         class="block mt-10 lg:m-0 w-full border-b flex-1 focus:outline-none h-8"
     >
-    <input
-        type="submit"
-        value="odoslať ->"
-        class="bg-transparent text-xl cursor-pointer -ml-4 mt-4 lg:m-0 px-4 focus:outline-none"
-    >
+    <button class="group flex text-xl px-4 -ml-4 mt-4 lg:m-0">
+        odoslať
+        <span class="ml-2 transform group-hover:translate-x-3 duration-200">
+            →
+        </span>
+    </button>
 </form>
