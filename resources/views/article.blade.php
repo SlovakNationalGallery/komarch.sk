89f62213fdfac39ff2a0de786a4256e9bb94b745
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-27 19:56:37 +0200

    Simple article route with controller added

---
+++ resources/views/article.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('title', 'správy')
+
+@section('content')
+
+<x-header.header></x-header.header>
+
+<div class="container mx-auto px-4">
+    This is an article
+</div>
+
+<x-footer.footer></x-footer.footer>
+
+@endsection
