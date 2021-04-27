31b4047dcefbc8907d566aeaca7cdbf95b1f42ca
Andrej Bilec <andrej.bilec@gmail.com>
2021-04-27 20:15:45 +0200

    Article header structure progress

--- resources/views/components/article/article-header.blade.php
+++ resources/views/components/article/article-header.blade.php
@@ -1,3 +1,11 @@
-<div class="container-article mx-auto">
-    <h1 class="text-xl tracking-tight">This is an article</h1>
-</div>
+<header class="container-article mx-auto">
+    <img src="https://placekitten.com/640/360" alt="cat">
+    <div class="flex mt-16">
+        <span>Tag</span>
+        <span>Publish Date</span>
+        <span class="ml-auto">email</span>
+        <span>link</span>
+        <span>facebook</span>
+    </div>
+    <h1 class="text-xl tracking-tight mt-14">This is an article</h1>
+</header>
