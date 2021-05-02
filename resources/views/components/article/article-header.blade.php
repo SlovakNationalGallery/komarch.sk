d4fde339f1651156e05044f96428ab6d6395e3a7
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-02 15:37:18 +0200

    Main post content displayed on the page

--- resources/views/components/article/article-header.blade.php
+++ resources/views/components/article/article-header.blade.php
@@ -1,11 +1,11 @@
 <header class="container-article mx-auto">
     <img src="https://placekitten.com/640/360" alt="cat">
     <div class="flex mt-16">
-        <span>Tag</span>
-        <span>Publish Date</span>
+        <span>{{ $post['tags'][0]['name'] }}</span>
+        <span>{{ $post['published_at'] }}</span>
         <span class="ml-auto">email</span>
         <span>link</span>
         <span>facebook</span>
     </div>
-    <h1 class="text-xl tracking-tight mt-14">This is an article</h1>
+    <h1 class="text-xl tracking-tight mt-14">{{ $post['title'] }}</h1>
 </header>
