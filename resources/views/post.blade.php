ab01718f7c7a50a8ee0cc0fc46d6c43f1b5ea146
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-04 18:38:42 +0200

    OG Meta tags for posts added

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
-@section('title', 'správy')
+
+@section('title')
+    {{ $post['title'] }}
+@stop
+
+<x-post.post-og :post="$post"></x-post.post-og>
 
 @section('content')
-
 <x-header.header></x-header.header>
-
 <div class="container mx-auto px-4">
     <x-post.post-back :post="$post"></x-post.post-back>
     <x-post.post-header :post="$post"></x-post.post-header>
     <x-post.post-content :post="$post"></x-post.post-content>
     <x-post.post-related :post="$post"></x-post.post-related>
 </div>
-
 <x-footer.footer></x-footer.footer>
-
-@endsection
+@stop
