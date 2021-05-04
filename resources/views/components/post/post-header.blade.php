d474147ca7f3c098eb2996bfd5bb2ce2dc8f38af
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-04 19:04:37 +0200

    Post share links added

--- resources/views/components/post/post-header.blade.php
+++ resources/views/components/post/post-header.blade.php
@@ -4,9 +4,21 @@
             <span class="mr-4">{{ $tag['name'] }}</span>
         @endforeach
         <span>{{ $post['published_at'] }}</span>
-        <span class="ml-auto">email</span>
-        <span>link</span>
-        <span>facebook</span>
+        <a
+            class="ml-auto"
+            href="mailto:?to=&body={{ Request::url() }}"
+            target="_blank"
+        >
+            email
+        </a>
+        <a class="ml-4">link</a>
+        <a
+            class="ml-4"
+            href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}"
+            target="_blank"
+        >
+            facebook
+        </a>
     </div>
     <h1 class="text-xl tracking-tight mt-14">{{ $post['title'] }}</h1>
 </header>
