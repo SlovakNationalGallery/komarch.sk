c6a6cdb4a6cdc0350a000c34dfd746c987c8a34a
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-09 13:51:02 +0200

    Share icons added

--- resources/views/components/post/post-header-share.blade.php
+++ resources/views/components/post/post-header-share.blade.php
@@ -4,11 +4,11 @@
     href="mailto:?to=&body={{ Request::url() }}"
     target="_blank"
 >
-    <span class="group-hover:hidden">i-mail</span>
+    <span class="group-hover:hidden icon-mail"></span>
     <span class="hidden group-hover:block">{{ __('post.share_mail')}}</span>
 </a>
 <a class="group ml-4 w-20 flex justify-center cursor-pointer hover:text-blue">
-    <span class="group-hover:hidden">i-copy</span>
+    <span class="group-hover:hidden icon-link"></span>
     <span class="hidden group-hover:block">{{ __('post.share_copy')}}</span>
 </a>
 <a
@@ -16,6 +16,6 @@
     href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}"
     target="_blank"
 >
-    <span class="group-hover:hidden">i-fb</span>
+    <span class="group-hover:hidden icon-share"></span>
     <span class="hidden group-hover:block">{{ __('post.share_facebook')}}</span>
 </a>
