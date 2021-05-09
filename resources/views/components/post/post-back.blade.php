b8155cfaf19f6e84930e7efba69015bf14835012
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-09 12:51:42 +0200

    Basic article back link added

--- resources/views/components/post/post-back.blade.php
+++ resources/views/components/post/post-back.blade.php
@@ -1,3 +1,8 @@
 <div>
-    This is some precontent
+    <div class="text-xl">
+        Informácie SKA
+    </div>
+    <a href="/spravy" target="_self" class="block mt-2">
+        ⟵ Späť na všetky informácie
+    </a>
 </div>
