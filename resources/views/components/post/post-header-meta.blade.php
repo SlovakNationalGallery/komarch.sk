468888df7941b7b15734917ff59f7ef422882ffd
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-09 12:53:29 +0200

    Article date format fixed

--- resources/views/components/post/post-header-meta.blade.php
+++ resources/views/components/post/post-header-meta.blade.php
@@ -1,4 +1,6 @@
 @foreach ($post['tags'] as $tag)
     <x-tag-hash :tag="$tag"></x-tag-hash>
 @endforeach
-<span>{{ $post['published_at'] }}</span>
+<span class="ml-12">
+    {{ $post['published_at']->formatLocalized('%d %B %Y') }}
+</span>
