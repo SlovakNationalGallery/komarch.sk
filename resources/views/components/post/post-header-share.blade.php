2a4fc345a054effea323564e26f030b9e96b88ee
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-09 14:06:52 +0200

    Share icons fontsize fixed

--- resources/views/components/post/post-header-share.blade.php
+++ resources/views/components/post/post-header-share.blade.php
@@ -1,21 +1,20 @@
-<!-- TODO: add share icons when they get added to the font -->
 <a
-    class="group ml-auto w-20 flex justify-center hover:text-blue"
+    class="group w-20 flex justify-center items-center hover:text-blue ml-auto"
     href="mailto:?to=&body={{ Request::url() }}"
     target="_blank"
 >
-    <span class="group-hover:hidden icon-mail"></span>
+    <span class="group-hover:hidden text-xl icon-mail"></span>
     <span class="hidden group-hover:block">{{ __('post.share_mail')}}</span>
 </a>
-<a class="group w-20 flex justify-center cursor-pointer hover:text-blue">
-    <span class="group-hover:hidden icon-link"></span>
+<a class="group w-20 flex justify-center items-center cursor-pointer hover:text-blue">
+    <span class="group-hover:hidden text-xl icon-link"></span>
     <span class="hidden group-hover:block">{{ __('post.share_copy')}}</span>
 </a>
 <a
-    class="group w-20 flex justify-center hover:text-blue"
+    class="group w-20 flex justify-center items-center hover:text-blue"
     href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}"
     target="_blank"
 >
-    <span class="group-hover:hidden icon-share"></span>
+    <span class="group-hover:hidden text-xl icon-share"></span>
     <span class="hidden group-hover:block">{{ __('post.share_facebook')}}</span>
 </a>
