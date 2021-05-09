f6c8d621a88cdae840577ab9c0317c5225899abd
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-09 13:53:27 +0200

    Spacing in sharing buttons fixed

--- resources/views/components/post/post-header-share.blade.php
+++ resources/views/components/post/post-header-share.blade.php
@@ -7,12 +7,12 @@
     <span class="group-hover:hidden icon-mail"></span>
     <span class="hidden group-hover:block">{{ __('post.share_mail')}}</span>
 </a>
-<a class="group ml-4 w-20 flex justify-center cursor-pointer hover:text-blue">
+<a class="group w-20 flex justify-center cursor-pointer hover:text-blue">
     <span class="group-hover:hidden icon-link"></span>
     <span class="hidden group-hover:block">{{ __('post.share_copy')}}</span>
 </a>
 <a
-    class="group ml-4 w-20 flex justify-center hover:text-blue"
+    class="group w-20 flex justify-center hover:text-blue"
     href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}"
     target="_blank"
 >
