22a51b7329d64f81853690093adeb0a612e3600b
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-11 17:06:04 +0200

    Basic big post teaser added

--- resources/js/components/SwiperPosts.vue
+++ resources/js/components/SwiperPosts.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        {{ posts }}
+        <TeaserPostBig v-for="post in posts" :post="post" />
     </div>
 </template>
 
 <script>
+  import TeaserPostBig from "./TeaserPostBig";
+
   export default {
+      components: { TeaserPostBig },
       props: {
           posts: {
               type: Array,
---
+++ resources/js/components/TeaserPostBig.vue
@@ -0,0 +1,32 @@
+<template>
+    <article class="py-10 grid grid-cols-2 place-items-start items-center">
+        <TagHash :tag="post.tags[0]"></TagHash>
+        <TagDate>{{ post.published_at }}</TagDate>
+        <h3 class="mt-1 text-xl col-span-2 tracking-tight">
+            <LinkTitle :url="post.slug">
+                {{ post.title }}
+            </LinkTitle>
+        </h3>
+        <p class="col-span-2">{{ post.perex.sk }}</p>
+    </article>
+</template>
+
+<script>
+import TagHash from "./atoms/tags/TagHash";
+import TagDate from "./atoms/tags/TagDate";
+import LinkTitle from "./atoms/links/LinkTitle";
+
+  export default {
+      components: {
+          TagHash,
+          TagDate,
+          LinkTitle
+      },
+      props: {
+          post: {
+              type: Object,
+              required: true
+          },
+      },
+  }
+</script>
